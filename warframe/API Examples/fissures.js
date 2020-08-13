54461f97369f8875e55b8f2cee2a78a97b8c36fe
Effend Wang <effend_wang@outlook.com>
2020-08-13 23:46:07 +0800

    Warframe API examples

---
+++ warframe/API Examples/fissures.js
@@ -0,0 +1,163 @@
+// Data on current fissures
+// Information about current Void Fissure misssions
+// https://api.warframestat.us/pc/fissures
+
+// API示例
+[
+  {
+    "node": "string",
+    "expired": true,
+    "eta": "string",
+    "missionType": "string",
+    "tier": "string",
+    "tierNum": 0,
+    "enemy": "string",
+    "id": "string",
+    "expiry": "2020-07-25T00:38:36Z",
+    "activation": "2020-07-25T00:38:36Z"
+  }
+]
+
+// 实际示例
+[
+    {
+        'id': '5f1bd16adc6a12e3c0490caf', 
+        'activation': '2020-07-25T06:30:01.996Z', 
+        'startString': '-1h 1s', 
+        'expiry': '2020-07-25T08:28:25.704Z', 
+        'active': True, 
+        'node': 'Garus (Kuva Fortress)', 
+        'missionType': 'Rescue', 
+        'enemy': 'Grineer', 
+        'tier': 'Requiem', 
+        'tierNum': 5, 
+        'expired': False, 
+        'eta': '58m 22s'
+    }, 
+    {
+        'id': '5f1bd25a47e0129e01beadfb', 
+        'activation': '2020-07-25T06:34:02.002Z', 
+        'startString': '-56m 1s', 
+        'expiry': '2020-07-25T08:01:25.992Z', 
+        'active': True, 
+        'node': 'Gaia (Earth)', 
+        'missionType': 'Interception', 
+        'enemy': 'Grineer', 
+        'tier': 'Lith', 
+        'tierNum': 1, 
+        'expired': False, 
+        'eta': '31m 22s'
+    }, 
+    {
+        'id': '5f1bd25a47e0129e01beadfc', 
+        'activation': '2020-07-25T06:34:02.002Z', 
+        'startString': '-56m 1s', 
+        'expiry': '2020-07-25T07:43:27.275Z', 
+        'active': True, 
+        'node': 'Unda (Venus)', 
+        'missionType': 'Spy', 
+        'enemy': 'Corpus', 
+        'tier': 'Lith', 
+        'tierNum': 1, 
+        'expired': False, 
+        'eta': '13m 24s'
+    }, 
+    {
+        'id': '5f1bd691f619aaf25c96e67f', 
+        'activation': '2020-07-25T06:52:01.789Z', 
+        'startString': '-38m 1s', 
+        'expiry': '2020-07-25T08:50:00.202Z', 
+        'active': True, 
+        'node': 'Nimus (Eris)', 
+        'missionType': 'Survival', 
+        'enemy': 'Infested', 
+        'tier': 'Axi', 
+        'tierNum': 4, 
+        'expired': False, 
+        'eta': '1h 19m 57s'
+    }, 
+    {
+        'id': '5f1bd691f619aaf25c96e680', 
+        'activation': '2020-07-25T06:52:01.789Z', 
+        'startString': '-38m 1s', 
+        'expiry': '2020-07-25T08:04:29.551Z', 
+        'active': True, 
+        'node': 'Naga (Sedna)', 
+        'missionType': 'Rescue', 
+        'enemy': 'Grineer', 
+        'tier': 'Axi', 
+        'tierNum': 4, 
+        'expired': False, 
+        'eta': '34m 26s'
+    }, 
+    {
+        'id': '5f1bd6cd3505c1781e853ad1', 
+        'activation': '2020-07-25T06:53:01.921Z', 
+        'startString': '-37m 1s', 
+        'expiry': '2020-07-25T08:38:27.744Z', 
+        'active': True, 
+        'node': 'Telesto (Saturn)', 
+        'missionType': 'Extermination', 
+        'enemy': 'Grineer', 
+        'tier': 'Meso', 
+        'tierNum': 2, 
+        'expired': False, 
+        'eta': '1h 8m 24s'
+    }, 
+    {
+        'id': '5f1bd6cd3505c1781e853ad2', 
+        'activation': '2020-07-25T06:53:01.921Z', 
+        'startString': '-37m 1s', 
+        'expiry': '2020-07-25T08:07:41.388Z', 
+        'active': True, 
+        'node': 'Kiste (Ceres)', 
+        'missionType': 'Mobile Defense', 
+        'enemy': 'Grineer', 
+        'tier': 'Meso', 
+        'tierNum': 2, 
+        'expired': False, 
+        'eta': '37m 38s'
+    }, 
+    {
+        'id': '5f1bd6cd3505c1781e853ad3', 
+        'activation': '2020-07-25T06:53:01.921Z', 
+        'startString': '-37m 1s', 
+        'expiry': '2020-07-25T08:46:12.736Z', 
+        'active': True, 
+        'node': 'Elara (Jupiter)', 
+        'missionType': 'Survival', 
+        'enemy': 'Corpus', 
+        'tier': 'Meso', 
+        'tierNum': 2, 
+        'expired': False, 
+        'eta': '1h 16m 9s'
+    }, 
+    {
+        'id': '5f1bda15c4916de304d05301', 
+        'activation': '2020-07-25T07:07:01.363Z', 
+        'startString': '-23m 1s', 
+        'expiry': '2020-07-25T08:56:39.089Z', 
+        'active': True, 
+        'node': 'Galatea (Neptune)', 
+        'missionType': 'Capture', 
+        'enemy': 'Corpus', 
+        'tier': 'Neo', 
+        'tierNum': 3, 
+        'expired': False, 
+        'eta': '1h 26m 36s'
+    }, 
+    {
+        'id': '5f1bda15c4916de304d05302', 
+        'activation': '2020-07-25T07:07:01.363Z', 
+        'startString': '-23m 1s', 
+        'expiry': '2020-07-25T08:33:40.645Z', 
+        'active': True, 
+        'node': 'Valefor (Europa)', 
+        'missionType': 'Excavation', 
+        'enemy': 'Corpus', 
+        'tier': 'Neo', 
+        'tierNum': 3, 
+        'expired': False, 
+        'eta': '1h 3m 37s'
+    }
+]
