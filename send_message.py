66dc7e40bcc107913071e8c35b331c9c7e0d7b54
Effend Wang <effend_wang@outlook.com>
2020-08-13 23:37:36 +0800

    Create message and send to QQ frame

---
+++ send_message.py
@@ -0,0 +1,93 @@
+# 该文档代码完成发送消息的操作
+
+#data={
+#	"message_type":message_type,
+#	"cq_status":cq_status,
+#	"cq_id":cq_id,
+#	"group_id":group_id,
+#	"user_id":user_id,
+#	"nickname":nickname,
+#	"message":message,
+#	"image_url":image_url,
+#	"message_status":message_status
+#}
+
+import requests
+import chat_authority
+import warframe.query_info
+
+# 消息发送模块主程序
+def send(self_id,data):
+
+	# 模块初始化
+	api_ip="http://127.0.0.1:8080/"
+	private_api="send_private_msg"
+	group_api="send_group_msg"
+
+	# 消息初始化
+	message_type=data.get("message_type")
+	cq_status=data.get("cq_status")
+	cq_id=data.get("cq_id")
+	group_id=data.get("group_id")
+	user_id=data.get("user_id")
+	message=data.get("message")
+	image_url=data.get("image_url")
+
+
+
+	# 建立需要发送的数据包（仅当接收到私密消息或者@自己的群消息时）
+	if (message_type=="private"):
+		# 检查聊天权限
+		authority=chat_authority.get_authority(group_id,user_id)
+		if (authority==False):
+			return ""
+		message_to_send=message_create(message,authority)
+	elif (message_type=="group" and cq_status):
+		group_send_status=False
+		for i in range(len(cq_id)):
+			if (cq_id[i]==self_id):
+				group_send_status=True
+		if group_send_status:
+			# 检查聊天权限
+			authority=chat_authority.get_authority(group_id,user_id)
+			if (authority==False):
+				return ""
+			message_to_send=message_create(message,authority)
+			#print(message_to_send)
+		else:
+			return ""
+	else:
+		return ""
+
+	if (message_to_send=="" or message_to_send==[]):
+		return ""
+
+	# 发送消息
+	if (message_type=="private"):
+		api_url=api_ip+private_api
+		data={
+			"user_id":user_id,
+			"message":message_to_send,
+			"auto_escape":False
+		}
+		r=requests.post(api_url,data=data)
+	elif (message_type=="group"):
+		api_url=api_ip+group_api
+		data={
+			"group_id":group_id,
+			"message":"[CQ:at,qq="+str(user_id)+"]"+message_to_send,
+			"auto_escape":False
+		}
+		r=requests.post(api_url,data=data)
+	#print(r.text)
+
+def message_create(message_from_user,authority):
+
+	if authority.get("warframe"):
+		message_to_send=warframe.query_info.get_info(message_from_user)
+	elif authority.get("honkai3"):
+		return ""
+	else:
+		message_to_send==""
+
+	return message_to_send
