9465aa6eb2f3cfc7709e34f02850389b37748b86
Effend Wang <effend_wang@outlook.com>
2020-08-14 00:45:03 +0800

    Program test examples

--- Test Examples/get_allocsession.py
+++ Test Examples/get_allocsession.py
@@ -1,5 +1,6 @@
 # 该代码为发送群消息的示例代码
 import requests
+from json import loads
 
 # 需要发送的数据包
 data = {
@@ -9,10 +10,22 @@ data = {
 }
 
 # API接口地址
-api_url='http://127.0.0.1:8080/send_group_msg'
+session='http://127.0.0.1:10429/allocsession'
 
 # 发送数据，并接收回传数据
-r=requests.post(api_url,data=data)
+r=requests.post(session).text
 
 # 输出回传数据内容
-print(r.text)
+print(r)
+
+r=loads(r)
+session_id=r.get("session_id")
+print(session_id)
+req={
+	"sessid":session_id
+}
+
+while True:
+	getevent="http://127.0.0.1:10429/getevent"
+	data=requests.post(getevent,data=req)
+	print(data.text)
--- Test Examples/private_message.py
+++ Test Examples/private_message.py
@@ -1,15 +1,23 @@
 # 该代码为发送私密消息的示例代码
 import requests
+from urllib.parse import quote
+import string
 
 # 需要发送的数据包
+
+message='+-* /，。、？：；#$%^&*(@)!“”中文'
+message=quote(message)
+print(message)
+print(message)
+
 data = {
-	'user_id':123456789,				# 发送的私密对象ID
-	'message':'我是一个可爱的小机器人喵',	# 发送的消息内容
-	'auto_escape':False					# 自动回传
+	"fromqq":3501530063,
+	'toqq':465152177,				# 发送的私密对象ID
+	'text':message,	# 发送的消息内容
 }
 
 # API接口地址
-api_url='http://127.0.0.1:8080/send_private_msg'
+api_url='http://127.0.0.1:10429/sendprivatemsg'
 
 # 发送数据并接收传回数据
 r=requests.post(api_url,data=data)
