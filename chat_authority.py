ecec33e8a7a2630f6e10712fe5c9aeebf3d78414
Effend Wang <effend_wang@outlook.com>
2020-08-14 00:55:23 +0800

    Chat authority control

--- chat_authority.py
+++ chat_authority.py
@@ -2,6 +2,7 @@
 # 其中get_list()保存权限信息，包括：
 #	"id":<string>群号或个人号；
 #	"type":<string>号码类型（"group"群号码，"private"个人号码）；
+#	"admin":<bool>是否有管理员权限；
 #	"warframe":<bool>是否有Warframe模块聊天权限；
 #	"honkai3":<bool>是否有崩坏三模块聊天权限；
 
@@ -14,7 +15,7 @@ def get_list():
 			"admin":<bool>,
 			"warframe":<bool>,
 			"honkai3":<bool>
-		},
+		}
 	]
 
 	return chat_list
