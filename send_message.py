c86e4bdc0695e0950cb12211b23c191bd0138d64
Effend Wang <effend_wang@outlook.com>
2020-08-14 00:56:37 +0800

    Create and send message to QQ frame

--- send_message.py
+++ send_message.py
@@ -12,6 +12,7 @@
 #	"message_status":message_status
 #}
 
+import urllib
 import requests
 import chat_authority
 from warframe.warframe_operate import warframe
@@ -20,14 +21,14 @@ from warframe.warframe_operate import warframe
 def send(self_id,data):
 
 	# 模块初始化
-	api_ip="http://127.0.0.1:8080/"
-	private_api="send_private_msg"
-	group_api="send_group_msg"
+	api_ip="http://127.0.0.1:10429/"
+	private_api="sendprivatemsg"
+	group_api="sendgroupmsg"
 
 	# 消息初始化
 	message_type=data.get("message_type")
-	cq_status=data.get("cq_status")
-	cq_id=data.get("cq_id")
+	at_status=data.get("at_status")
+	at_id=data.get("at_id")
 	group_id=data.get("group_id")
 	user_id=data.get("user_id")
 	message=data.get("message")
@@ -40,11 +41,11 @@ def send(self_id,data):
 		if (authority==False):
 			return ""
 		message_to_send=message_create(message,authority)
-	#elif (message_type=="group" and cq_status):
+	#elif (message_type=="group" and at_status):
 	elif (message_type=="group"):
 		#group_send_status=False
-		#for i in range(len(cq_id)):
-			#if (cq_id[i]==self_id):
+		#for i in range(len(at_id)):
+			#if (at_id[i]==self_id):
 				#group_send_status=True
 		group_send_status=True
 		if group_send_status:
@@ -66,17 +67,17 @@ def send(self_id,data):
 	if (message_type=="private"):
 		api_url=api_ip+private_api
 		data={
-			"user_id":user_id,
-			"message":message_to_send,
-			"auto_escape":False
+			"fromqq":self_id,
+			"toqq":user_id,
+			"text":message_to_send
 		}
 		r=requests.post(api_url,data=data)
 	elif (message_type=="group"):
 		api_url=api_ip+group_api
 		data={
-			"group_id":group_id,
-			"message":"[CQ:at,qq="+str(user_id)+"]"+message_to_send,
-			"auto_escape":False
+			"fromqq":self_id,
+			"togroup":group_id,
+			"text":"[@"+str(user_id)+"] "+message_to_send
 		}
 		r=requests.post(api_url,data=data)
 	#print(r.text)
